6d9013599ef92ebd135f17a91848fd08845eb238
sukmuzi <sukmuzi@naver.com>
2023-09-02 04:19:07 +0900

    rename: 상품 조회 응답 클래스명 변경

---
+++ src/main/java/com/ongshop/api/response/product/ProductResponse.java
@@ -0,0 +1,25 @@
+package com.ongshop.api.response.product;
+
+import com.ongshop.domain.Product;
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+    private Long no;
+    private String title;
+    private String price;
+    private String category;
+    private String[] option;
+    private String explanation;
+    private String imgUrl;
+
+    public ProductResponse(Product product) {
+        this.no = product.getNo();
+        this.title = product.getTitle();
+        this.price = product.getPrice();
+        this.category = product.getCategory();
+        this.option = product.getOption().split(", ");
+        this.explanation = product.getExplanation();
+        this.imgUrl = product.getImgUrl();
+    }
+}
